4f75da7f341ae57440816658d20c9e3b42981a17
moriMaroo17 <morimaroo17@gmail.com>
2021-10-05 00:13:37 +0300

    added doesn't spend gas func

--- zombiehelper.sol
+++ zombiehelper.sol
@@ -4,8 +4,38 @@ pragma solidity <0.9.0;
 import "./ZombieFeeding.sol";
 
 contract ZombieHelper is ZombieFeeding {
-    modifier aboveLevel(uint _level, uint _zombieId) {
+    modifier aboveLevel(uint256 _level, uint256 _zombieId) {
         require(zombies[_zombieId].level >= _level);
         _;
     }
+
+    function changeName(uint256 _zombieId, string memory _newName)
+        external
+        aboveLevel(2, _zombieId)
+    {
+        require(msg.sender == zombieToOwner[_zombieId]);
+        zombies[_zombieId].name = _newName;
+    }
+
+    function changeDna(uint256 _zombieId, uint256 _newDna)
+        external
+        aboveLevel(20, _zombieId)
+    {
+        require(msg.sender == zombieToOwner[_zombieId]);
+      	zombies[_zombieId].dna = _newDna;
+    }
+
+	function getZombiesByOwner(address _owner) external view returns (uint[] memory) {
+		uint[] memory result = new uint[](ownerZombieCount[_owner]);
+
+		uint counter = 0;
+		for (uint i = 0; i < zombies.length; i++) {
+			if (zombieToOwner[i] == _owner) {
+				result[counter] = i;
+				counter++;
+			}
+		}
+		return result;
+
+	}
 }
