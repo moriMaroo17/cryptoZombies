9e54f1bb1a83510df7ddf1335bfa7cb1b4e74b25
moriMaroo17 <morimaroo17@gmail.com>
2021-10-04 21:09:58 +0300

    .gitignore fixed

--- ZombieFactory.sol
+++ ZombieFactory.sol
@@ -18,19 +18,20 @@ contract ZombieFactory {
     mapping (uint => address) public zombieToOwner;
     mapping (address => uint) ownerZombieCount;
 
-    function _createZombie(string _name, uint _dna) internal {
-        uint id = zombies.push(Zombie(_name, _dna)) - 1;
+    function _createZombie(string memory _name, uint _dna) internal {
+        zombies.push(Zombie(_name, _dna));
+        uint id = zombies.length - 1;
         zombieToOwner[id] = msg.sender;
         ownerZombieCount[msg.sender]++;
-        NewZombie(id, _name, _dna);
+        emit NewZombie(id, _name, _dna);
     } 
 
-    function _generateRandomDna(string _str) private view returns (uint) {
-        uint rand = uint(keccak256(_str));
+    function _generateRandomDna(string memory _str) private view returns (uint) {
+        uint rand = uint(keccak256(abi.encode(_str)));
         return rand % dnaModulus;
     }
 
-    function createRandomZombie(string _name) public {
+    function createRandomZombie(string memory _name) public {
         require(ownerZombieCount[msg.sender] == 0);
         uint randDna = _generateRandomDna(_name);
         _createZombie(_name, randDna);
--- ZombieFeeding.sol
+++ ZombieFeeding.sol
@@ -3,8 +3,8 @@ pragma solidity <0.9.0;
 import "./ZombieFactory.sol";
 
 
-contract KittyInterface {
-    function getKitty(uint256 _id) external view returns (
+abstract contract KittyInterface {
+    function getKitty(uint256 _id) external virtual view returns (
     bool isGestating,
     bool isReady,
     uint256 cooldownIndex,
@@ -24,12 +24,12 @@ contract ZombieFeeding is ZombieFactory {
     address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
     KittyInterface kittyContract = KittyInterface(ckAddress);
     
-    function feedAndMultiply(uint _zombieId, uint _targetDna, string _species) public {
+    function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) public {
         require(msg.sender == zombieToOwner[_zombieId]);
         Zombie storage myZombie = zombies[_zombieId];
         _targetDna = _targetDna % dnaModulus;
         uint newDna = (myZombie.dna + _targetDna) / 2;
-        if (keccak256(_species) == keccak256("kitty")) {
+        if (keccak256(abi.encode(_species)) == keccak256(abi.encode("kitty"))) {
             newDna = newDna - newDna % 100 + 99;
         }
         _createZombie("NoName", newDna);
