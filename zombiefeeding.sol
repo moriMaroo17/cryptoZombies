d0f648ebcd78ac7efa576be6c5e899b95b6c620f
moriMaroo17 <morimaroo17@gmail.com>
2021-10-06 15:06:46 +0300

    added like-ERC721 contract ZombieOwnership

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity <0.9.0;
 
-import "./ZombieFactory.sol";
+import "./zombiefactory.sol";
 
 abstract contract KittyInterface {
     function getKitty(uint256 _id)
---
+++ zombieownership.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity <0.9.0;
+
+import "./zombieattack.sol";
+import "./erc721.sol";
+
+contract ZombieOwnership is ZombieAttack, ERC721 {
+
+}
