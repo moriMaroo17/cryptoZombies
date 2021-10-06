7ef85626c4f377ac7892eb802cf473a2239de977
moriMaroo17 <morimaroo17@gmail.com>
2021-10-06 14:50:40 +0300

    renamed some files

---
+++ zombiefeeding.sol
@@ -0,0 +1,67 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity <0.9.0;
+
+import "./ZombieFactory.sol";
+
+abstract contract KittyInterface {
+    function getKitty(uint256 _id)
+        external
+        view
+        virtual
+        returns (
+            bool isGestating,
+            bool isReady,
+            uint256 cooldownIndex,
+            uint256 nextActionAt,
+            uint256 siringWithId,
+            uint256 birthTime,
+            uint256 matronId,
+            uint256 sireId,
+            uint256 generation,
+            uint256 genes
+        );
+}
+
+contract ZombieFeeding is ZombieFactory {
+    
+    KittyInterface kittyContract;
+
+    modifier ownerOf(uint256 _zombieId) {
+        require(msg.sender == zombieToOwner[_zombieId]);
+        _;
+    }
+
+    function setKittyContractAddress(address _address) external onlyOwner {
+        kittyContract = KittyInterface(_address);
+    }
+
+    function _triggerCooldown(Zombie storage _zombie) internal {
+        _zombie.readyTime = uint32(block.timestamp + cooldownTime);
+    }
+
+    function _isReady(Zombie storage _zombie) internal view returns (bool) {
+        return (_zombie.readyTime <= block.timestamp);
+    }
+
+    function feedAndMultiply(
+        uint256 _zombieId,
+        uint256 _targetDna,
+        string memory _species
+    ) internal ownerOf(_zombieId) {
+        Zombie storage myZombie = zombies[_zombieId];
+        require(_isReady(myZombie));
+        _targetDna = _targetDna % dnaModulus;
+        uint256 newDna = (myZombie.dna + _targetDna) / 2;
+        if (keccak256(abi.encode(_species)) == keccak256(abi.encode("kitty"))) {
+            newDna = newDna - (newDna % 100) + 99;
+        }
+        _createZombie("NoName", newDna);
+        _triggerCooldown(myZombie);
+    }
+
+    function feedOnKitty(uint256 _zombieId, uint256 _kittyId) public {
+        uint256 kittyDna;
+        (, , , , , , , , , kittyDna) = kittyContract.getKitty(_kittyId);
+        feedAndMultiply(_zombieId, kittyDna, "kitty");
+    }
+}
--- zombiehelper.sol
+++ zombiehelper.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity <0.9.0;
 
-import "./ZombieFeeding.sol";
+import "./zombiefeeding.sol";
 
 contract ZombieHelper is ZombieFeeding {
 
