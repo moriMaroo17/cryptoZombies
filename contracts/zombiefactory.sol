9496a04b3b3e5dcdcc52ffa8b78ace75b0001243
grigory <morimaroo17@gmail.com>
2021-10-07 11:26:06 +0300

    made structure refactoring by adding folders for each files type

--- contracts/zombiefactory.sol
+++ contracts/zombiefactory.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity <0.9.0;
 
-import "node_modules/@openzeppelin/contracts/access/Ownable.sol";
+import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
 
 contract ZombieFactory is Ownable {
 
